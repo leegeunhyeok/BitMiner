5fc9507578200122f5a101a347d08acdf1d9c2c3
Leegeunhyeok <lghlove0509@naver.com>
2018-06-16 23:29:29 +0900

    Add comment

--- js/renderer.js
+++ js/renderer.js
@@ -4,19 +4,26 @@ const fs = require('fs')
 
 /* 파일 저장, 불러오기 클래스 */
 class Store {
-  /* Electron 유저 데이터 경로 저장 */
+  /**
+   * @description 클래스 생성자
+   * @param {string} fileName 세이브 파일 명
+   */
   constructor (fileName) {
     this.userDataPath = (electron.app || electron.remote.app).getPath('userData')
     this.userData = null
     this.fileName = fileName
   }
 
-  /* 불러오기 */
+  /**
+   * @description 세이브파일 불러오기
+   */
   load () {
     console.log(path.join(this.userDataPath, this.fileName))
   }
 
-  /* 저장 */
+  /**
+   * @description 세이브파일 저장하기
+   */
   save () {
     if (this.userData) {
       fs.writeFileSync(path.join(this.userDataPath, this.fileName), JSON.stringify(this.userData))
@@ -25,7 +32,9 @@ class Store {
     }
   }
 
-  /* 파일 생성 */
+   /**
+   * @description 세이브파일 새로 생성
+   */
   create () {
     // TODO: 새 파일 생성
   }
@@ -36,18 +45,24 @@ class Game {
     this.store = new Store('user.dat')
   }
 
-  /* 초기화 */
+  /**
+   * @description 게임 초기화
+   */
   init () {
     this.store.load()
   }
 }
 
+/* DOM 로딩 완료 후 */
 window.onload = function () {
+  /* 게임 인스턴스 생성 */
   const game = new Game()
+  /* 초기화 */
   game.init()
 
-  /* 버튼 이벤트 등록 */
+  /* 종료 버튼 이벤트 등록 */
   document.getElementById('exit-button').onclick = function () {
+    /* Electron 종료 */
     electron.remote.getCurrentWindow().close()
   }
 }
