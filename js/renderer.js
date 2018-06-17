f293eaeb6725d9313db5c6a05c5ce22653861cad
Leegeunhyeok <lghlove0509@naver.com>
2018-06-17 17:19:22 +0900

    Add button listener, Dialog UI

--- js/renderer.js
+++ js/renderer.js
@@ -1,68 +1,51 @@
 const electron = require('electron')
-const path = require('path')
-const fs = require('fs')
-
-/* 파일 저장, 불러오기 클래스 */
-class Store {
-  /**
-   * @description 클래스 생성자
-   * @param {string} fileName 세이브 파일 명
-   */
-  constructor (fileName) {
-    this.userDataPath = (electron.app || electron.remote.app).getPath('userData')
-    this.userData = null
-    this.fileName = fileName
-  }
-
-  /**
-   * @description 세이브파일 불러오기
-   */
-  load () {
-    console.log(path.join(this.userDataPath, this.fileName))
-  }
-
-  /**
-   * @description 세이브파일 저장하기
-   */
-  save () {
-    if (this.userData) {
-      fs.writeFileSync(path.join(this.userDataPath, this.fileName), JSON.stringify(this.userData))
-    } else {
-      console.log('유저 데이터가 없습니다.')
-    }
-  }
-
-   /**
-   * @description 세이브파일 새로 생성
-   */
-  create () {
-    // TODO: 새 파일 생성
-  }
-}
-
-class Game {
-  constructor () {
-    this.store = new Store('user.dat')
-  }
-
-  /**
-   * @description 게임 초기화
-   */
-  init () {
-    this.store.load()
-  }
-}
 
 /* DOM 로딩 완료 후 */
 window.onload = function () {
-  /* 게임 인스턴스 생성 */
-  const game = new Game()
-  /* 초기화 */
+  const Game = require('./game.js')
+  const game = new Game('user.dat')
   game.init()
+
+  /* 다이얼로그 유형(새 게임시작, 아이템 구매 여부 등) */
+  var dialogType = ''
+
+  /* 이어서하기 버튼 이벤트 등록 */
+  document.getElementById('continue-button').onclick = function () {
+    game.continue()
+  }
+
+  /* 새로하기 이벤트 등록 */
+  document.getElementById('new-button').onclick = function () {
+    /* 다이얼로그 창 띄우기 */
+    document.getElementById('dialog-message').textContent = '정말 새로 시작하시겠습니까?'
+    document.getElementById('dialog').style['display'] = 'block'
+    dialogType = 'newGame'
+  }
+
+  /* 정보 버튼 이벤트 등록 */
+  document.getElementById('info-button').onclick = function () {
+    console.log('정보 버튼 눌림')
+  }
 
   /* 종료 버튼 이벤트 등록 */
   document.getElementById('exit-button').onclick = function () {
     /* Electron 종료 */
     electron.remote.getCurrentWindow().close()
+  }
+  
+  /* 다이얼로그 예 버튼 이벤트 등록 */
+  document.getElementById('dialog-yes').onclick = function () {
+    /* 다이얼로그 창 닫기 */
+    document.getElementById('dialog').style['display'] = 'none'
+    
+    if (dialogType === 'newGame') {
+      game.newGame()
+    }
+  }
+
+  /* 다이얼로그 아니오 버튼 이벤트 등록 */
+  document.getElementById('dialog-no').onclick = function () {
+    /* 다이얼로그 창 닫기 */
+    document.getElementById('dialog').style['display'] = 'none'
   }
 }
